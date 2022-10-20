8877bb3506bcd8661c09642f1136019b9050f513
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-20 11:55:42 +0500

    is_even.py

--- is_even.py
+++ is_even.py
@@ -1,3 +1,9 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
+import numbers
+
+
+number = 7
+number = number + 1
+print(number % 2)
