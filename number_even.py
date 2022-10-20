cc1648054036cc19d0861414f680bb04c8497194
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-20 21:56:27 +0500

    misol

--- number_even.py
+++ number_even.py
@@ -3,3 +3,9 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of even digits in the variable "var_int".
+var_int = 1234
+x = ( var_int % 10 ) % 2
+y = ( ( var_int // 10 ) % 10 ) % 2
+z = ( ( ( var_int // 10 ) // 10 ) % 10 ) % 2
+d = ( var_int // 1000 ) % 2 
+print(4-(x+y+z+d))
